60af8a3ae48f7602ff70c6e244dc00737f608a73
Lê Đức Dũng <224736507+daryllee1004@users.noreply.github.com>
2026-01-30 15:28:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,15 +8,18 @@ const cards = [
   { id:'revealer', name:'Tiết lộ', group:'dan', img:'./assets/revealer.webp', count:0 },
   { id:'witch', name:'Phù thủy', group:'dan', img:'./assets/witch.webp', count:0 },
   { id:'seer', name:'Tiên tri', group:'dan', img:'./assets/seer.webp', count:0 },
-
   { id:'mentalist', name:'Tâm linh', group:'dan', img:'./assets/mentalist.webp', count:0 },
   { id:'tough', name:'Cứng cỏi', group:'dan', img:'./assets/tough_guy.webp', count:0 },
   { id:'prince', name:'Hoàng tử', group:'dan', img:'./assets/prince.webp', count:0 },
   { id:'priest', name:'Tu sĩ', group:'dan', img:'./assets/priest.webp', count:0 },
+  { id:'empathist', name:'Thấu cảm', group:'dan', img:'./assets/empathist.webp', count:0 },
+  { id:'insomniac', name:'Mất ngủ', group:'dan', img:'./assets/insomniac.webp', count:0 },
+  { id:'spellcaster', name:'Liễu', group:'dan', img:'./assets/spellcaster.webp', count:0 },
+  { id:'martyr', name:'Thiếu nữ', group:'dan', img:'./assets/martyr.webp', count:0 },
+  { id:'twin', name:'Song sinh', group:'dan', img:'./assets/twins.webp', count:0 },
 
   { id:'villager', name:'Dân làng', group:'dan', img:'./assets/villager.webp', count:0 },
   { id:'werewolf', name:'Ma sói', group:'soi', img:'./assets/werewolf.webp', count:0 },
-  { id:'twin', name:'Song sinh', group:'dan', img:'./assets/twin.webp', count:0 },
 ];
 
 let deck = [];
